26ab0f7a297a331c7056ff840d1797dc96ee19ce
Eric Schmiedeberg <flydecahedron@gmail.com>
2016-12-05 14:12:07 -0500

    Added events header

---
+++ src/Events.hpp
@@ -0,0 +1,19 @@
+/*
+ * Events.hpp
+ *
+ *  Created on: Dec 4, 2016
+ *      Author: eroc
+ */
+
+#ifndef EVENTS_HPP_
+#define EVENTS_HPP_
+
+#include <SFML/Audio.hpp>
+
+struct PlaySound {
+	explicit PlaySound()
+}; // PlaySound struct
+
+
+
+#endif /* EVENTS_HPP_ */
